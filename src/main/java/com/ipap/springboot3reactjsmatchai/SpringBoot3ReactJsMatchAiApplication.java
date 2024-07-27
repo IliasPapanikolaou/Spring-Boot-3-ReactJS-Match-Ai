4936e8c5125955a2c420be1e3f9f4acff3486c2b
IliasPapanikolaou <eliaspapanikolaou@hotmail.com>
2024-07-27 13:55:49 +0300

    Init commit

---
+++ src/main/java/com/ipap/springboot3reactjsmatchai/SpringBoot3ReactJsMatchAiApplication.java
@@ -0,0 +1,13 @@
+package com.ipap.springboot3reactjsmatchai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3ReactJsMatchAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot3ReactJsMatchAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipap/springboot3reactjsmatchai/SpringBoot3ReactJsMatchAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipap.springboot3reactjsmatchai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3ReactJsMatchAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
